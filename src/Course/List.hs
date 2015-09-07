91c07ff2ae027331a020d07f810a8b1f464b0326
Sonia Hamilton <sonia@snowfrog.net>
2015-09-07 14:27:39 +1000

    find

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -259,12 +259,16 @@ seqOptional =
 --
 -- >>> find (const True) infinity
 -- Full 0
+--
+-- foldRight :: (a -> b -> b) -> b -> List a -> b
+--
 find ::
   (a -> Bool)
   -> List a
   -> Optional a
-find =
-  error "todo: Course.List#find"
+find f =
+  foldRight g Empty where
+    g p q = if f p then (Full p) else q
 
 -- | Determine if the length of the given list is greater than 4.
 --
