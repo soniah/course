91f5df5c620d67463b2e791ae540dea0ab2493d3
Sonia Hamilton <sonia@snowfrog.net>
2015-09-16 19:43:02 +1000

    is

--- src/Course/Parser.hs
+++ src/Course/Parser.hs
@@ -308,8 +308,7 @@ satisfy fn =
 -- /Tip:/ Use the @satisfy@ function.
 is ::
   Char -> Parser Char
-is =
-  error "todo: Course.Parser#is"
+is c = satisfy (== c)
 
 -- | Return a parser that produces a character between '0' and '9' but fails if
 --
