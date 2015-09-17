a59c0625c886ff52ef9aa37108cf86af5a66a41a
Sonia Hamilton <sonia@snowfrog.net>
2015-09-17 10:50:18 +1000

    spaces1

--- src/Course/Parser.hs
+++ src/Course/Parser.hs
@@ -376,7 +376,7 @@ space = satisfy isSpace
 spaces1 ::
   Parser Chars
 spaces1 =
-  error "todo: Course.Parser#spaces1"
+  list1 space
 
 -- | Return a parser that produces a lower-case character but fails if
 --
